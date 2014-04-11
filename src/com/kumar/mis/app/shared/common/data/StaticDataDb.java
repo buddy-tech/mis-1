58f6e18592b349bf09c139d025e5a8698b64ee7d
Kumar Jeyaprakasam <jobsitejeya@gmail.com>
2014-04-11 08:08:24 +0530

    Git ignore and state data db.

---
+++ src/com/kumar/mis/app/shared/common/data/StaticDataDb.java
@@ -0,0 +1,45 @@
+package com.kumar.mis.app.shared.common.data;
+
+import java.util.HashMap;
+
+import com.google.gwt.user.client.rpc.core.java.util.Collections;
+
+
+public class StaticDataDb {
+
+	private static HashMap<String, String> cities = new HashMap<String, String>();
+	private static HashMap<String, String> states = new HashMap<String, String>();
+	private static HashMap<String, String> countries = new HashMap<String, String>();
+
+	static {
+		cities.put("Chennai", "Chennai");
+		cities.put("Trichy", "Trichy");
+		cities.put("Madurai", "Madurai");
+		cities.put("Others", "Others");
+		
+		states.put("TamilNadu","TamilNadu");
+		states.put("Andhra Pradesh","Andhra Pradesh");
+		states.put("Kerala","Kerala");
+		states.put("Maharastra","Maharastra");
+		states.put("Others","Others");
+		
+		countries.put("India", "India");
+		countries.put("USA", "USA");
+		countries.put("UK", "UK");
+	}
+
+	public static HashMap<String, String> getStates() {
+
+		return states;
+
+	}
+	
+	public static HashMap<String, String> getCountries(){
+		return countries;
+	}
+
+	public static HashMap<String, String> getCities() {
+		return cities;
+	}
+
+}
